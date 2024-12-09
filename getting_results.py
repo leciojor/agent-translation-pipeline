33267c1ba00cc82a28bd7aea510842fe760542bc
leciojor <leciobyu@gmail.com>
2024-12-08 18:40:54 -0700

    tsp

--- getting_results.py
+++ getting_results.py
@@ -33,7 +33,7 @@ def testing(tests, mode, lang):
                             initial = final_output[0]
                             translation = final_output[1]['refined_translation']
 
-                            tsv_writer.writerow([sentence, initial, translation])
+                            tsv_writer.writerow([sentence[0:-1], initial, translation])
                     except Exception as e:
                         tsv_writer.writerow([sentence, f"Error: {e}", f"Error: {e}"])
 
@@ -45,8 +45,8 @@ def main():
     '''
     options_llm_mode = [{"name": "comparisons/results/gpt4_22.tsv", "model":"", "k_models":2, "k":2}, {"name": "comparisons/results/gpt4_23.tsv", "k_models":2, "model":"", "k":3}, {"name": "comparisons/results/gpt4_14.tsv", "model":"", "k_models":1, "k":4}]
     options_nmt_mode = [{"name": "comparisons/results/nmtBing.tsv", "model":"", "k_models":1, "k":3, "nmt":"bing"}, {"name": "comparisons/results/nmtYandex.tsv", "model":"", "k_models":1, "k":3, "nmt":"yandex"}, {"name": "comparisons/results/nmtAlibaba.tsv", "k_models":1, "model":"", "k":3, "nmt":"alibaba"}]
-    # testing([options_llm_mode[2]], agent_translation, "german")
-    testing([options_nmt_mode[2]], system_translation, "german")
+    testing([options_llm_mode[0]], agent_translation, "german")
+    # testing([options_nmt_mode[2]], system_translation, "german")
 
 
 if __name__ == "__main__":
